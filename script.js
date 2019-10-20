7436f0352acb3edb6a0c9cadabca917b118aa56c
Mridul <iammridul2000@gmail.com>
2019-10-20 20:27:46 +0530

    error fixed

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-alert(console.log(signUpButton));
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
